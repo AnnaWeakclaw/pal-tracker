a8caf7d70cb30d4334711b8cb5589ddb23b8dea7
Anna Wieclaw <awieclaw@ford.com>
2021-03-08 13:05:11 +0000

    Commit from Mac

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -3,12 +3,12 @@ package io.pivotal.pal.tracker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-    @RestController
-    public class WelcomeController {
+@RestController
+public class WelcomeController {
 
-        @GetMapping("/")
-        public String sayHello() {
-            return "hello";
-        }
+    @GetMapping("/")
+    public String sayHello() {
+        return "hello";
     }
+}
 
